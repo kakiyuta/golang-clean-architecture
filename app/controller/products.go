a62f5a26f43d1939ed013fa07658144992648262
y-kakinuma <y-kakinuma@paronym.jp>
2024-07-17 13:20:16 +0900

    不要なコードを削除

--- app/controller/products.go
+++ app/controller/products.go
@@ -11,35 +11,6 @@ import (
 )
 
 func (c *Controller) GetV1Products(ctx echo.Context, params api.GetV1ProductsParams) error {
-	// products := []*api.Product{
-	// 	{
-	// 		Id:   Int64Ptr(1),
-	// 		Name: StringPtr("product1"),
-	// 		Validations: &[]api.Validation{
-	// 			{
-	// 				Id:    Int64Ptr(1),
-	// 				Name:  StringPtr("validation1"),
-	// 				Price: IntPtr(100),
-	// 			},
-	// 			{
-	// 				Id:    Int64Ptr(2),
-	// 				Name:  StringPtr("validation2"),
-	// 				Price: IntPtr(100),
-	// 			},
-	// 		},
-	// 	},
-	// 	{
-	// 		Id:   Int64Ptr(2),
-	// 		Name: StringPtr("product2"),
-	// 		Validations: &[]api.Validation{
-	// 			{
-	// 				Id:    Int64Ptr(3),
-	// 				Name:  StringPtr("validation3"),
-	// 				Price: IntPtr(100),
-	// 			},
-	// 		},
-	// 	},
-	// }
 
 	input := input.NewGetProducts(params.Limit, params.Offset)
 	output, err := usecase.NewProducts().GetProducts(*input)
